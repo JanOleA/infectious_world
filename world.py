cba42d2a6fedabaf5d047ed49de8a6ea9e0f5304
joaakerh <joaakerh@student.matnat.uio.no>
2020-05-23 02:31:47 +0200

    movement added

--- world.py
+++ world.py
@@ -5,18 +5,24 @@ from matplotlib import animation
 from PIL import Image
 import random
 import time
+import names
+
 
 class World:
     def __init__(self, in_map):
         in_map = in_map[::-1]
         self._map_define_types = ["p", "r", "h", "w", "c"]
-        self._map_define_colors = np.array([[28,167,0],[121,121,121],[177,115,18],[72,94,255],[149,0,167]])
+        self._map_define_colors = np.array([[28,167,0], # park, green
+                                            [121,121,121], # road, grey
+                                            [177,115,18], # home, brown
+                                            [255,0,0], # work, red
+                                            [149,0,167]]) # common area, purple
 
         self.generate_map(in_map)
         self._num_actors = len(self._actors_list)
         self.initialize_inhabitans()
 
-
+    
     def generate_map(self, in_map):
         print("Generating map...")
         self._map = []
@@ -50,7 +56,7 @@ class World:
                     new_object = Road(x, y)
                     self._road_list.append(new_object)
                 elif char == "h":
-                    new_object = House(x, y, (1, 3))
+                    new_object = House(x, y, 1)
                     self._house_list.append(new_object)
                 elif char == "w":
                     new_object = Work(x, y)
@@ -78,14 +84,16 @@ class World:
             for item in row:
                 item.set_roadconnections(self._map, self._world_size)
 
-            if isinstance(item, House) or isinstance(item, Work):
-                if len(item.roadconnections) == 0:
-                    print(f"House/workplace at position {item.position} has no road connections")
+                if isinstance(item, House) or isinstance(item, Work):
+                    if len(item.roadconnections) == 0:
+                        print(f"House/workplace at position {item.position} has no road connections")
 
 
     def initialize_inhabitans(self):
         print(f"Inhabitants = {self._num_actors}")
         print("Initializing actors...")
+
+        self._actor_positions = []
         
         s = ""
         for i, actor in enumerate(self._actors_list):
@@ -94,12 +102,36 @@ class World:
             print(s, end = "\r")
             workplace = np.random.choice(self._workplace_list)
             actor.set_workplace(self._map, workplace)
+            self._actor_positions.append(actor.position)
+
         print(" "*len(s), end = "\r")
         print("100%")
 
+        self._actor_positions = np.array(self._actor_positions, dtype=np.float64)
 
-    def plot_world(self):
-        fig, ax = plt.subplots(figsize = (11,11))
+
+    def frame_forward(self):
+        """ Step one frame forward in the simulation """
+        for i, actor in enumerate(self._actors_list):
+            actor_pos = actor.frame_forward()
+            self._actor_positions[i] = actor_pos
+
+
+    def send_N_to_work(self, N):
+        inds = np.random.randint(self._num_actors, size = N)
+        for ind in inds:
+            actor = self._actors_list[ind]
+            actor.set_motion(self._map, actor.workplace)
+
+    
+    def get_actor_positions(self):
+        return self._actor_positions.copy()
+
+
+    def plot_world(self, ax = None):
+        """ Plot the world in its current state """
+        if ax is None:
+            fig, ax = plt.subplots(figsize = (8,8))
         for i, line in enumerate(self._map):
             for j, item in enumerate(line):
                 params = item.plot_params
@@ -113,54 +145,17 @@ class World:
                                          edgecolor = params["edgecolor"])
 
                 ax.add_patch(rect)
-
-
-        for actor in self._actors_list:
-            pos = actor.position
-            params = actor.plot_params
-            xrand = (np.random.random() - 0.5)/2.5
-            yrand = (np.random.random() - 0.5)/2.5
-            plt.plot(pos[0] + xrand, pos[1] + yrand,
-                     marker = params["shape"],
-                     color = params["color"],
-                     markersize = params["markersize"])
         
         ax.set_ylim(-1, self._world_size[0])
         ax.set_xlim(-1, self._world_size[1])
         
         plt.axis("equal")
 
-    
-    def find_random_path(self):
-        """ For testing pathfinding. Chooses a random person and a random destination and tries to find a path """
-        start = np.random.choice(self._actors_list)
-        destination = np.random.choice(self._workplace_list)
-
-        path = start.find_path(self._map, destination)
-        return start, destination, path
-        
-    
-    def plot_random_path(self):
-        start, destination, path = self.find_random_path()
-        if not path:
-            plt.plot(start.position[0], start.position[1], marker = "x", color = "red", markersize = 15)
-            plt.plot(destination.position[0], destination.position[1], marker = "x", color = "blue", markersize = 15)
-            return False
-        else:
-            plt.plot(start.position[0], start.position[1], marker = "x", color = "red")
-            plt.plot(destination.position[0], destination.position[1], marker = "x", color = "blue")
-            
-            cur_pos = start.position
-            for node in path[1:]:
-                next_pos = node.position
-                plt.plot([cur_pos[0], next_pos[0]], [cur_pos[1], next_pos[1]], color = "black")
-                cur_pos = next_pos
-
 
 class MapObject:
     """ Superclass for objects on the map """
     def __init__(self, x, y):
-        self._params = {}
+        self._params = {} # plot params
         self._params["x-size"] = 1
         self._params["y-size"] = 1
         self._roadconnections = [] # list of tuples that point to adjacent roads in (x, y) format
@@ -171,11 +166,25 @@ class MapObject:
         self.gscore = np.inf # used for pathfinding
         self.fscore = np.inf # used for pathfinding
 
+        self._contained_actors = []
+
+    
+    def add_actor(self, actor):
+        """ Add an actor to the ones within this object """
+        if actor not in self._contained_actors:
+            self._contained_actors.append(actor)
+
+
+    def remove_actor(self, actor):
+        """ Remove an actor from this object, if it is in it """
+        while actor in self._contained_actors:
+            self._contained_actors.remove(actor)
+
     
     @property
     def position(self):
         """ return position as (x, y) format """
-        return (self._x, self._y)
+        return np.array((self._x, self._y))
 
 
     def set_roadconnections(self, map_, world_size):
@@ -299,7 +308,7 @@ class Work(MapObject):
 
     @property
     def plot_params(self):
-        self._params["color"] = "red"
+        self._params["color"] = "tomato"
         self._params["edgecolor"] = "black"
 
         return self._params
@@ -344,6 +353,7 @@ class Actor:
         self._params = {}
         self._x = self._xhome
         self._y = self._yhome
+        self._current_container = homeplace
 
         self._status = "idle"
 
@@ -354,30 +364,89 @@ class Actor:
         self._xwork, self._ywork = workplace.position
         self._workplace = workplace
 
-        workpath = self.find_path(map_, workplace)
+        workpath = self.find_path(map_, workplace, self.homeplace)
         if workpath != False:
             self._workpath = workpath
         else:
-            print("Could not find workpath for", self)
+            print("Could not find workpath for", self, f"living at {self.position}")
+
+
+    def set_motion(self, map_, target):
+        self._status = "moving"
+        current_path = None
+        if target == self._workplace:
+            if self._current_container in self._workpath:
+                current_path = self._workpath
+                path_ind = current_path.index(self._current_container)
+        elif target == self._homeplace:
+            if self._current_container in self._workpath:
+                current_path = self._workpath[::-1]
+                path_ind = current_path.index(self._current_container)
+        
+        if current_path is None:
+            current_path = self.find_path(map_, target)
+            path_ind = 0
+            
+        self._target_node = current_path[path_ind + 1]
+        self._path_ind = path_ind
+        self._current_path = current_path
+
+
+    def frame_forward(self):
+        """ move the actor along the current path if status is 'moving'"""
+        if self._status == "moving":
+            target_vector = self._target_node.position - self.position
+            target_dist = np.linalg.norm(target_vector)
+
+            direction = target_vector/np.linalg.norm(target_dist)
+            
+            new_pos = self.position + direction*0.2
+            current_node_dist = np.linalg.norm(self.position - self._current_container.position)
+            if target_dist < current_node_dist:
+                if self._target_node == self._current_path[-1]:
+                    if target_dist < 0.3:
+                        """ target reached """
+                        self._x, self._y = self._target_node.position
+                        self._current_container.remove_actor(self)
+                        self._current_container = self._target_node
+                        self._current_container.add_actor(self)
+                        self._status = "idle"
+                        self._target_node = None
+                        self._path_ind = None
+                        self._current_path = None
+                    else:
+                        self._x, self._y = new_pos[0], new_pos[1]
+                    return self.position
+
+                self._path_ind += 1
+                self._current_container.remove_actor(self)
+                self._current_container = self._target_node
+                self._current_container.add_actor(self)
+                self._target_node = self._current_path[self._path_ind + 1]
+
+            self._x, self._y = new_pos[0], new_pos[1]
+            return self.position
+        else:
+            return self.position
 
 
     def set_color(self, color):
         self._params["color"] = color
     
 
-    def find_path(self, map_, target):
+    def find_path(self, map_, target, start = None):
         """ A* algorithm for pathfinding """
         target_position = target.position
-        array_position = np.array([self._x, self._y])
-        array_target_position = np.array(target_position)
-
-        i, j = self._y, self._x
-
-        start = map_[i,j]
+        if start is None:
+            start_position = self.position
+            i, j = int(round(self._y)), int(round(self._x))
+            start = map_[i,j]
+        else:
+            start_position = start.position
 
         start.camefrom = None
         start.gscore = 0
-        start.fscore = self._h(array_position, array_target_position)
+        start.fscore = self._h(start_position, target_position)
         open_set = [start]
         open_set_fscores = [start.fscore]
         closed_set = []
@@ -410,12 +479,12 @@ class Actor:
                 if tentative_gscore < neighbor.gscore:
                     neighbor.camefrom = current
                     neighbor.gscore = tentative_gscore
-                    neighbor.fscore = neighbor.gscore + self._h(neighbor_position, array_target_position)
+                    neighbor.fscore = neighbor.gscore + self._h(neighbor_position, target_position)
                     if neighbor not in open_set:
                         open_set.append(neighbor)
                         open_set_fscores.append(neighbor.fscore)
 
-        return False
+        return False # if no path was found
 
 
     def _init_params(self):
@@ -443,23 +512,55 @@ class Actor:
         return path[::-1]
 
 
+    def plot_workpath(self):
+        """ Plots the path from home to work for the this actor """
+        plt.plot(self.homeplace.position[0], self.homeplace.position[1], marker = "x", color = "red")
+        plt.plot(self.workplace.position[0], self.workplace.position[1], marker = "x", color = "blue")
+        
+        path = self.workpath
+
+        cur_pos = self.homeplace.position
+        for node in path[1:]:
+            next_pos = node.position
+            plt.plot([cur_pos[0], next_pos[0]], [cur_pos[1], next_pos[1]], color = "black")
+            cur_pos = next_pos
+
     @property
     def position(self):
         """ Return (x, y) posititon of actor """
-        return (self._x, self._y)
+        return np.array((self._x, self._y))
 
-    
     @property
     def status(self):
         return self._status
 
+    @property
+    def homeplace(self):
+        return self._homeplace
+
+    @property
+    def workplace(self):
+        return self._workplace
+
+    @property
+    def workpath(self):
+        return self._workpath
+
 
 class Person(Actor):
+    def __init__(self, xhome, yhome, homeplace):
+        super().__init__(xhome, yhome, homeplace)
+        self._name = names.get_full_name()
+
+
     def _init_params(self):
         self._params["color"] = "cyan"
         self._params["shape"] = "^"
         self._params["markersize"] = 1
 
+    
+    def __str__(self):
+        return self._name
 
     @property
     def plot_params(self):
@@ -472,5 +573,8 @@ if __name__ == "__main__":
     world = World(np.array(im))
     print("Plotting...")
     world.plot_world()
+    world._actors_list[0].set_motion(world._map, world._actors_list[0].workplace)
+    for i in range(200):
+        world.frame_forward()
     plt.show()
 
